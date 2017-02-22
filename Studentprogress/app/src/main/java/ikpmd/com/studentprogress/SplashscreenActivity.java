2384f63a202b58b6bad7655229f5ff96b1732ba4
Ying Kun Zhan <yingkun.zhan@serviceheroes.com>
2017-02-22 23:12:19 +0100

    Added splashscreen and launcher icon

---
+++ Studentprogress/app/src/main/java/ikpmd/com/studentprogress/SplashscreenActivity.java
@@ -0,0 +1,25 @@
+package ikpmd.com.studentprogress;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class SplashscreenActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
