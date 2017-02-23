fac382d7163276bb26099a318bae156d92588a15
Ying Kun Zhan <yingkun.zhan@serviceheroes.com>
2017-02-24 00:43:00 +0100

    Changed showCourses query

---
+++ StudentProgress/app/src/main/java/ikpmd/com/studentprogress/Fragments/MainResultFragment.java
@@ -0,0 +1,44 @@
+package ikpmd.com.studentprogress.Fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import ikpmd.com.studentprogress.R;
+
+
+public class MainResultFragment extends Fragment {
+    public MainResultFragment() {
+    }
+
+    public static MainResultFragment newInstance(String param1, String param2) {
+        MainResultFragment fragment = new MainResultFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main_result, container, false);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+}
