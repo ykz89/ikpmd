f4e1c0edfe67a8734f9b0bd6ab4ff1427c32fe38
Ying Kun Zhan <yingkun.zhan@serviceheroes.com>
2017-02-28 03:56:26 +0100

    made it possible to insert null

--- api/updateGrade.php
+++ api/updateGrade.php
@@ -14,7 +14,7 @@ function updateGrade()
         $grade = mysqli_real_escape_string($connect, $grade);
         $name = mysqli_real_escape_string($connect, $name);
 
-        $query = "UPDATE Course SET grade = '$grade' WHERE name = '$name';";
+        $query = "UPDATE Course SET grade = $grade WHERE name = '$name';";
         mysqli_query($connect, $query) or die(mysqli_error($connect));
         mysqli_close($connect);
         echo "success";
